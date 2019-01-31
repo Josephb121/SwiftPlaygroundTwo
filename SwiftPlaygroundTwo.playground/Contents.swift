569fade9355eeb04f6e4e66694128874a6df60cc
CM Student <cmstudent@uhwo-acm-13.local>
2019-01-31 10:21:07 -1000

    Completed Swift Playgrounds Two

---
+++ SwiftPlaygroundTwo.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+var str = "Hello, playground"
+//Part 3 Start
+var goodGrade = "I've been doing all my work"
+//first if statement
+if goodGrade == "I've been doing all my work"
+{
+    
+    print("I should be getting a good grade")
+    
+
+}
+
+//Part 3 #2
+
+if goodGrade == "I haven't been doing all my work"
+{
+    
+    print("I should be getting a good grade")
+}
+
+//Part 4: If/else statements
+if goodGrade == "I've been doing all my work"{
+    print("I should be getting an A at this point")
+}else{
+    print("I need to work harder in this class")
+}
+// the other Part 4: Using Boolean Variables in if statements
+var gradeA = true
+if gradeA{
+    print("I really like this class")
+}
+else
+{
+    print("I'm not so sure about coding")
+}
